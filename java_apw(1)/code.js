c7793a1fb6ee91a000d4ea3587bb8f0f536b8f83
Mariana Campello <125597199+marianacampelo@users.noreply.github.com>
2023-09-29 08:57:44 -0300

    aula 1

---
+++ java_apw(1)/code.js
@@ -0,0 +1,9 @@
+function imprime(){
+    let lg = document.getElementById("login").value
+    let ps = document.getElementById("pass").value 
+
+    let senha = "mari1"
+    if (ps == senha){
+        alert("Você esstá logado!")
+    }
+}
